7787e49cd53e22d74d5ee71ff855fc23408ccea1
edmreis280-art <edm.reis280@gmail.com>
2025-12-12 20:49:43 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,7 @@
 const flashcards = [
   { front: "Hello", back: "Olá" },
   { front: "Dog", back: "Cachorro" },
-  { front: "House", back: "Casa" }
+  { front: "House", back: "Casa" },
+  { front: "Book", back: "Livro" },
+  { front: "Water", back: "Água" }
 ];
