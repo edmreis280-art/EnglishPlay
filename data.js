a749289de61e2806315e8910f0e1d412ca65aaec
edmreis280-art <edm.reis280@gmail.com>
2025-12-25 21:44:29 -0300

    Delete data.js

--- data.js
+++
@@ -1,7 +0,0 @@
-const flashcards = [
-  { front: "Hello", back: "Olá" },
-  { front: "Dog", back: "Cachorro" },
-  { front: "House", back: "Casa" },
-  { front: "Book", back: "Livro" },
-  { front: "Water", back: "Água" }
-];
